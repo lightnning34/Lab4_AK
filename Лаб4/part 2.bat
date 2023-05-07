14a2939e691361c96d1484894102c88af57666c9
lightnning34 <132834083+lightnning34@users.noreply.github.com>
2023-05-07 22:44:14 +0200

    Update part 2.bat

--- Лаб4/part 2.bat
+++ Лаб4/part 2.bat
@@ -10,6 +10,23 @@ IF "%1"=="" (
 set "source=%~1"
 set "destination=%~2"
 
+
+attrib "%file_name%" | find "H" >nul
+if %errorlevel% equ 0 (
+    echo Файл %file_name% имеет атрибут "скрытый".
+)
+
+attrib "%file_name%" | find "R" >nul
+if %errorlevel% equ 0 (
+    echo Файл %file_name% имеет атрибут "только для чтения".
+)
+
+attrib "%file_name%" | find "A" >nul
+if %errorlevel% equ 0 (
+    echo Файл %file_name% имеет атрибут "архивный".
+)
+
+
 move "%source%" "%destination%"
 
 echo Exit code 0
